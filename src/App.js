512b036875dd5ab059c66bcc228e21da9325f179
Aman Kashyap <amankashyap0246@gmail.com>
2023-04-18 10:37:39 +0530

    experiance coloum added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './component/Nav/Navbar';
 import Intro from './component/Intro/Intro';
 import Services from "./component/Services/Services";
+import Experiance from "./component/Experiance/Experiance"
  
 
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App"><Navbar/>
     <Intro/>
     <Services/>
+    <Experiance></Experiance>
     
     </div>
 
